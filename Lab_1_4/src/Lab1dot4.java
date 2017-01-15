4214c1de9a069661f1b012c0d04063c9eab03591
olegukr <oleg.poberezhets@gmail.com>
2017-01-15 09:32:53 +0200

    Initial commit

---
+++ Lab_1_4/src/Lab1dot4.java
@@ -0,0 +1,91 @@
+/**
+ * Created by olegpoberezhets on 15.01.17.
+ *
+ */
+
+
+public class Lab1dot4 {
+    public static void main(String[] args) {
+
+// Lab 1.4.1
+// Write a console program prints on console each of the primitive data types
+// (byte, short, int, long, float, double, char, boolean).
+
+        byte B1 = 4;
+        short S1 = 450;
+        int I1 = 23_000;
+        long L1 = 12_000_000;
+        float F1 = 2.123E2F;
+        double D1 = 5.11232;
+        char C1 = 'A';
+        boolean Boo1 = false;
+
+
+// Lab 1.4.2
+// Write a console program that prints result of each of arithmetic operations
+// (+, -, /, *, %) for two variables of primitive data types.
+        System.out.println("Lab 1.4.2");
+        byte B2 = 5;
+        short S2 = 370;
+        int I2 = 65_000;
+        long L2 = 7_000_000;
+        float F2 = 23.123E2F;
+        double D2 = 531.11232;
+        char C2 = 'B';
+        boolean Boo2 = true;
+
+        System.out.println("++++++++");
+        System.out.println((B1 + B2));
+        System.out.println((S1 + S2));
+        System.out.println((I1 + I2));
+        System.out.println((L1 + L2));
+        System.out.println((F1 + F2));
+        System.out.println((D1 + D2));
+        System.out.println((C1 + C2));
+//        System.out.println((Boo1 + Boo2));
+
+        System.out.println("--------");
+        System.out.println((B1 - B2));
+        System.out.println((S1 - S2));
+        System.out.println((I1 - I2));
+        System.out.println((L1 - L2));
+        System.out.println((F1 - F2));
+        System.out.println((D1 - D2));
+        System.out.println((C1 - C2));
+//        System.out.println((Boo1 + Boo1));
+
+        System.out.println("********");
+        System.out.println((B1 * B2));
+        System.out.println((S1 * S2));
+        System.out.println((I1 * I2));
+        System.out.println((L1 * L2));
+        System.out.println((F1 * F2));
+        System.out.println((D1 * D2));
+        System.out.println((C1 * C2));
+//        System.out.println((Boo1 + Boo2));
+
+        System.out.println("//////////");
+        System.out.println((B1 / B2));
+        System.out.println((S1 / S2));
+        System.out.println((I1 / I2));
+        System.out.println((L1 / L2));
+        System.out.println((F1 / F2));
+        System.out.println((D1 / D2));
+        System.out.println((C1 / C2));
+//        System.out.println((Boo1 + Boo2));
+
+        System.out.println("%%%%%%%%%%");
+        System.out.println((B1 % B2));
+        System.out.println((S1 % S2));
+        System.out.println((I1 % I2));
+        System.out.println((L1 % L2));
+        System.out.println((F1 % F2));
+        System.out.println((D1 % D2));
+        System.out.println((C1 % C2));
+//        System.out.println((Boo1 + Boo2));
+
+
+
+
+    }
+}
