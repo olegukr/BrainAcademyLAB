e404dc67fba401d3652978b3788ba14dbb2a81ae
olegukr <oleg.poberezhets@gmail.com>
2017-01-20 22:13:27 +0200

    12:49

--- Lab_1_4_5/src/Lab145.java
+++ Lab_1_4_5/src/Lab145.java
@@ -7,21 +7,22 @@
  */
 public class Lab145 {
     public static void main(String[] args) {
+
         int a = 0b0000_1100;
         int b = 0b0000_1100;
-        System.out.println("a: " + Integer.toBinaryString(a));
-        System.out.println("b: " + Integer.toBinaryString(b));
+        System.out.println("a:         " + Integer.toBinaryString(a));
+        System.out.println("b:         " + Integer.toBinaryString(b));
 
         System.out.println("==========");
         a >>= 2;
         b = b / 4;
-        System.out.println("a >> 2: " + Integer.toBinaryString(a));
-        System.out.println("b = b / 4: " + Integer.toBinaryString(b));
+        System.out.println("a >> 2:      " + Integer.toBinaryString(a));
+        System.out.println("b = b / 4:   " + Integer.toBinaryString(b));
 
         System.out.println("==========");
         a <<= 2;
         b = b * 4;
-        System.out.println("a << 2: " + Integer.toBinaryString(a));
+        System.out.println("a << 2:    " + Integer.toBinaryString(a));
         System.out.println("b = b * 4: " + Integer.toBinaryString(b));
     }
 }
